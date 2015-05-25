0166e7d971140c4a952005ab05dc81d71b5b5189
Sajith Sasidharan <sajith@nonzen.in>
2015-05-25 11:48:06 -0400

    Realign.

--- dot_xmonad/xmonad.hs
+++ dot_xmonad/xmonad.hs
@@ -7,31 +7,29 @@ import           XMonad.Util.EZConfig     (additionalKeys)
 import           XMonad.Util.Run          (spawnPipe)
 
 myManageHook = composeAll
-  [   className =? "Gimp"      --> doFloat
-    , className =? "Vncviewer" --> doFloat
-  ]
+               [ className =? "Gimp"      --> doFloat
+               , className =? "Vncviewer" --> doFloat
+               ]
 
 main = do
-  xmproc <- spawnPipe "xmobar ~/.xmobar/xmobarrc"
-  xmonad $ defaultConfig
-    { terminal           = "gnome-terminal"
-    , modMask            = mod4Mask
-    , borderWidth        = 2
-    , focusedBorderColor = "darkgreen"
-    , manageHook         = manageDocks <+> myManageHook
-                                       <+> manageHook defaultConfig
-    , layoutHook         = avoidStruts $ layoutHook defaultConfig
-    , logHook            = dynamicLogWithPP xmobarPP
-                           { ppOutput = hPutStrLn xmproc
-                           , ppTitle  = xmobarColor "green" "" . shorten 80
-                           }
-    , startupHook        = execScriptHook "startup"
-    } `additionalKeys`
-    [
-        -- ((mod4Mask .|. shiftMask, xK_l), spawn "xscreensaver-command -l")
-        -- , ((mod4Mask .|. shiftMask, xK_a), spawn "xscreensaver-command -a")
-        ((mod4Mask .|. shiftMask, xK_l), spawn "gnome-screensaver-command -l")
-      , ((mod4Mask .|. shiftMask, xK_a), spawn "gnome-screensaver-command -a")
-      , ((mod4Mask .|. shiftMask, xK_g), spawn "emacsclient -c -n &")
-    ]
-
+    xmproc <- spawnPipe "xmobar ~/.xmobar/xmobarrc"
+    xmonad $ defaultConfig
+        { terminal           = "gnome-terminal"
+        , modMask            = mod4Mask
+        , borderWidth        = 2
+        , focusedBorderColor = "darkgreen"
+        , manageHook         = manageDocks <+> myManageHook
+                               <+> manageHook defaultConfig
+        , layoutHook         = avoidStruts $ layoutHook defaultConfig
+        , logHook            = dynamicLogWithPP xmobarPP
+                               { ppOutput = hPutStrLn xmproc
+                               , ppTitle  = xmobarColor "green" "" . shorten 80
+                               }
+        , startupHook        = execScriptHook "startup"
+        } `additionalKeys`
+        [ -- ((mod4Mask .|. shiftMask, xK_l), spawn "xscreensaver-command -l")
+          -- , ((mod4Mask .|. shiftMask, xK_a), spawn "xscreensaver-command -a")
+          ((mod4Mask .|. shiftMask, xK_l), spawn "gnome-screensaver-command -l")
+        , ((mod4Mask .|. shiftMask, xK_a), spawn "gnome-screensaver-command -a")
+        , ((mod4Mask .|. shiftMask, xK_g), spawn "emacsclient -c -n &")
+        ]
